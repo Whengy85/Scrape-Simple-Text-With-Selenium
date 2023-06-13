d19d2409acdaec2e628c40b43e37e73d3fba9aab
WillisHengy <23218422-WillisHengy@users.noreply.replit.com>
2023-06-13 21:28:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("disable-infobars")
+options.add_argument("start-maximized")
+options.add_argument() # Left Off Here
+driver = webdriver.Chrome()
